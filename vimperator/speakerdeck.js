e4c739a9244f07c8b8cd5c1f71d5da9201e5b249
eagletmt <eagletmt@gmail.com>
2013-06-10 15:10:22 +0900

    vimperator/speakerdeck.js: Add "go" subcommand

--- vimperator/speakerdeck.js
+++ vimperator/speakerdeck.js
@@ -9,12 +9,26 @@
     }
   }
 
+  function slideCompleter(context, args) {
+    getPlayer(function(player) {
+      context.title = ['Slides', ''];
+      context.compare = void 0;
+      let n = player.count();
+      let ary = new Array(n);
+      for (let i = 0; i < n; i++) {
+        ary[i] = [i, ''];
+      }
+      context.completions = ary;
+    });
+  }
+
   commands.addUserCommand(['speakerdeck'], 'speakerdeck controller',
     function() {
     }, {
       subCommands: [
         new Command(['n[ext]'], 'Go next slide', function() getPlayer(function(player) player.nextSlide())),
         new Command(['p[rev]'], 'Go previous slide', function() getPlayer(function(player) player.previousSlide())),
+        new Command(['go'], 'Go to slide', function(args) getPlayer(function(player) player.goToSlide(parseInt(args[0], 10))), { argCount: 1, completer: slideCompleter }),
       ],
     }, true);
 })();
