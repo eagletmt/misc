bb29380a4972792b7eca030476e1579c3320401e
Kohei Suzuki <eagletmt@gmail.com>
2014-03-13 03:23:52 +0900

    packer/arch64: packer 0.3.11 has "guest_additions_mode" setting

--- packer/arch64/scripts/vagrant.sh
+++ packer/arch64/scripts/vagrant.sh
@@ -7,5 +7,3 @@ chmod 600 ~/.ssh/authorized_keys
 
 sudo pacman --noconfirm -S virtualbox-guest-utils
 sudo sh -c 'echo vboxsf > /etc/modules-load.d/vagrant-vbox.conf'
-
-rm -f ~/VBoxGuestAdditions.iso
