4b6481f2aea175b7da276841787c0205d129d7d4
Kohei Suzuki <eagletmt@gmail.com>
2017-02-24 23:59:51 +0900

    :wave: Vimperator

--- vimperator/lastpass.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  let klass = Cc['@lastpass.com/lastpass;1'];
-  if (klass) {
-    commands.addUserCommand(['lplogin'], 'Open LastPass login dialog', function() {
-      klass.getService().wrappedJSObject.lpGetCurrentWindow().openDialog('chrome://lastpass/content/login.xul', '_blank', 'resizable,chrome,toolbar,centerscreen,modal');
-    }, { argCount: '0' }, true);
-  }
-})();
