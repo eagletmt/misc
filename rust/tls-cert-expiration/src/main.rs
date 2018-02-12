2ca5c5c4b669455f27b80769fc64d008534e8fe4
Kohei Suzuki <eagletmt@gmail.com>
2018-02-12 23:13:32 +0900

    rust/tls-cert-expiration: Update dependencies

--- rust/tls-cert-expiration/src/main.rs
+++ rust/tls-cert-expiration/src/main.rs
@@ -29,13 +29,18 @@ fn main() {
 
     let threshold = chrono::Local::now() + chrono::Duration::days(30);
 
-    let connector = openssl::ssl::SslConnectorBuilder::new(openssl::ssl::SslMethod::tls())
-        .expect("SslConnectorBuilder::new failed")
+    let connector = openssl::ssl::SslConnector::builder(openssl::ssl::SslMethod::tls())
+        .expect("SslConnector::builder failed")
         .build();
-    let tcp_stream = std::net::TcpStream::connect((&*host, port))
-        .expect("TcpStream::connect failed");
-    let tls_stream = connector.connect(&*host, tcp_stream).expect("SslConnector::connect failed");
-    let cert = tls_stream.ssl().peer_certificate().expect("No peer_certificate");
+    let tcp_stream =
+        std::net::TcpStream::connect((&*host, port)).expect("TcpStream::connect failed");
+    let tls_stream = connector
+        .connect(&*host, tcp_stream)
+        .expect("SslConnector::connect failed");
+    let cert = tls_stream
+        .ssl()
+        .peer_certificate()
+        .expect("No peer_certificate");
     let not_after = from_asn1_time(cert.not_after()).expect("Unable to parse not_after");
     let not_before = from_asn1_time(cert.not_before()).expect("Unable to parse not_before");
     println!("{}", host);
@@ -51,9 +56,9 @@ fn print_usage(program: &str, options: &getopts::Options) {
     println!("{}", options.usage("Check TLS certificate expiration"));
 }
 
-
-fn from_asn1_time(t: &openssl::asn1::Asn1TimeRef)
-                  -> Result<chrono::DateTime<chrono::Local>, chrono::ParseError> {
+fn from_asn1_time(
+    t: &openssl::asn1::Asn1TimeRef,
+) -> Result<chrono::DateTime<chrono::Local>, chrono::ParseError> {
     chrono::DateTime::parse_from_str(&t.to_string().replace(" GMT", " +00:00"), "%b %e %T %Y %z")
         .map(|in_utc| in_utc.with_timezone(&chrono::Local))
 }
