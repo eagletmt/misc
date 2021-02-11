c51035f596ad6ecaa770da3975db0f6e10eb710d
Kohei Suzuki <eagletmt@gmail.com>
2021-02-12 02:00:29 +0900

    rust/miam2tf: Use writer

--- rust/miam2tf/src/main.rs
+++ rust/miam2tf/src/main.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), anyhow::Error> {
     let miam = miam2tf::loader::load_miam("IAMfile")?;
-    miam2tf::printer::print_as_hcl2(&miam);
+    miam2tf::printer::print_as_hcl2(&mut std::io::stdout().lock(), &miam)?;
     Ok(())
 }
--- rust/miam2tf/src/printer.rs
+++ rust/miam2tf/src/printer.rs
@@ -1,39 +1,49 @@
-pub fn print_as_hcl2(miam: &crate::Miam) {
+pub fn print_as_hcl2<W>(writer: &mut W, miam: &crate::Miam) -> Result<(), std::io::Error>
+where
+    W: std::io::Write,
+{
     for user in &miam.users {
-        println!(r#"resource "aws_iam_user" "{}" {{"#, user.user_name);
-        println!(r#"  name = "{}""#, user.user_name);
+        writeln!(writer, r#"resource "aws_iam_user" "{}" {{"#, user.user_name)?;
+        writeln!(writer, r#"  name = "{}""#, user.user_name)?;
         if let Some(ref path) = user.path {
-            println!(r#"  path = "{}""#, path);
+            writeln!(writer, r#"  path = "{}""#, path)?;
         }
-        println!("}}");
+        writeln!(writer, "}}")?;
 
         for policy in &user.policies {
-            println!(
+            writeln!(
+                writer,
                 r#"resource "aws_iam_user_policy" "{}-{}" {{"#,
                 user.user_name, policy.name
-            );
-            println!(r#"  name = "{}""#, policy.name);
-            println!("  user = aws_iam_user.{}.name", user.user_name);
-            println!(
+            )?;
+            writeln!(writer, r#"  name = "{}""#, policy.name)?;
+            writeln!(writer, "  user = aws_iam_user.{}.name", user.user_name)?;
+            writeln!(
+                writer,
                 "  policy = data.aws_iam_policy_document.{}-{}.json",
                 user.user_name, policy.name
-            );
-            println!("}}");
+            )?;
+            writeln!(writer, "}}")?;
 
-            print_policy_document(&format!("{}-{}", user.user_name, policy.name), &policy);
+            print_policy_document(
+                writer,
+                &format!("{}-{}", user.user_name, policy.name),
+                &policy,
+            )?;
         }
         if !user.groups.is_empty() {
-            println!(
+            writeln!(
+                writer,
                 r#"resource "aws_iam_user_group_membership" "{}" {{"#,
                 user.user_name,
-            );
-            println!("  user = aws_iam_user.{}.name", user.user_name);
-            println!("  groups = [");
+            )?;
+            writeln!(writer, "  user = aws_iam_user.{}.name", user.user_name)?;
+            writeln!(writer, "  groups = [")?;
             for group in &user.groups {
-                println!("    aws_iam_group.{}.name,", group);
+                writeln!(writer, "    aws_iam_group.{}.name,", group)?;
             }
-            println!("  ]");
-            println!("}}");
+            writeln!(writer, "  ]")?;
+            writeln!(writer, "}}")?;
         }
         for policy in &user.attached_managed_policies {
             let short_policy_name = policy.rsplitn(2, '/').next().unwrap_or_else(|| {
@@ -42,38 +52,45 @@ pub fn print_as_hcl2(miam: &crate::Miam) {
                     policy, user.user_name
                 )
             });
-            println!(
+            writeln!(
+                writer,
                 r#"resource "aws_iam_user_policy_attachment" "{}-{}" {{"#,
                 user.user_name, short_policy_name
-            );
-            println!("  user = aws_iam_user.{}.name", user.user_name);
-            println!("  policy_arn = aws_iam_policy.{}.arn", short_policy_name);
-            println!("}}");
+            )?;
+            writeln!(writer, "  user = aws_iam_user.{}.name", user.user_name)?;
+            writeln!(
+                writer,
+                "  policy_arn = aws_iam_policy.{}.arn",
+                short_policy_name
+            )?;
+            writeln!(writer, "}}")?;
         }
     }
 
     for group in &miam.groups {
-        println!(r#"resource "aws_iam_group" "{}" {{"#, group.name);
-        println!(r#"  name = "{}""#, group.name);
+        writeln!(writer, r#"resource "aws_iam_group" "{}" {{"#, group.name)?;
+        writeln!(writer, r#"  name = "{}""#, group.name)?;
         if let Some(ref path) = group.path {
-            println!(r#"  path = "{}""#, path);
+            writeln!(writer, r#"  path = "{}""#, path)?;
         }
-        println!("}}");
+        writeln!(writer, "}}")?;
 
         for policy in &group.policies {
-            println!(
+            writeln!(
+                writer,
                 r#"resource "aws_iam_group_policy" "{}-{}" {{"#,
                 group.name, policy.name
-            );
-            println!(r#"  name = "{}""#, policy.name);
-            println!("  user = aws_iam_group.{}.name", group.name);
-            println!(
+            )?;
+            writeln!(writer, r#"  name = "{}""#, policy.name)?;
+            writeln!(writer, "  user = aws_iam_group.{}.name", group.name)?;
+            writeln!(
+                writer,
                 "  policy = data.aws_iam_policy_document.{}-{}.json",
                 group.name, policy.name
-            );
-            println!("}}");
+            )?;
+            writeln!(writer, "}}")?;
 
-            print_policy_document(&format!("{}-{}", group.name, policy.name), &policy);
+            print_policy_document(writer, &format!("{}-{}", group.name, policy.name), &policy)?;
         }
         for policy in &group.attached_managed_policies {
             let short_policy_name = policy.rsplitn(2, '/').next().unwrap_or_else(|| {
@@ -82,51 +99,59 @@ pub fn print_as_hcl2(miam: &crate::Miam) {
                     policy, group.name
                 )
             });
-            println!(
+            writeln!(
+                writer,
                 r#"resource "aws_iam_group_policy_attachment" "{}-{}" {{"#,
                 group.name, short_policy_name
-            );
-            println!("  user = aws_iam_group.{}.name", group.name);
-            println!("  policy_arn = aws_iam_policy.{}.arn", short_policy_name);
-            println!("}}");
+            )?;
+            writeln!(writer, "  user = aws_iam_group.{}.name", group.name)?;
+            writeln!(
+                writer,
+                "  policy_arn = aws_iam_policy.{}.arn",
+                short_policy_name
+            )?;
+            writeln!(writer, "}}")?;
         }
     }
 
     for role in &miam.roles {
-        println!(r#"resource "aws_iam_role" "{}" {{"#, role.name);
-        println!(r#"  name = "{}""#, role.name);
+        writeln!(writer, r#"resource "aws_iam_role" "{}" {{"#, role.name)?;
+        writeln!(writer, r#"  name = "{}""#, role.name)?;
         if let Some(ref path) = role.path {
-            println!(r#"  path = "{}""#, path);
+            writeln!(writer, r#"  path = "{}""#, path)?;
         }
         if role.assume_role_policy_document.is_some() {
-            println!(
+            writeln!(
+                writer,
                 "  assume_role_policy = data.aws_iam_policy_document.assume-role-{}.json",
                 role.name
-            );
+            )?;
         }
         if let Some(duration) = role.max_session_duration {
-            println!("  max_session_duration = {}", duration);
+            writeln!(writer, "  max_session_duration = {}", duration)?;
         }
-        println!("}}");
+        writeln!(writer, "}}")?;
 
         if let Some(ref policy) = role.assume_role_policy_document {
-            print_policy_document(&role.name, &policy);
+            print_policy_document(writer, &role.name, &policy)?;
         }
 
         for policy in &role.policies {
-            println!(
+            writeln!(
+                writer,
                 r#"resource "aws_iam_role_policy" "{}-{}" {{"#,
                 role.name, policy.name
-            );
-            println!(r#"  name = "{}""#, policy.name);
-            println!("  user = aws_iam_role.{}.name", role.name);
-            println!(
+            )?;
+            writeln!(writer, r#"  name = "{}""#, policy.name)?;
+            writeln!(writer, "  user = aws_iam_role.{}.name", role.name)?;
+            writeln!(
+                writer,
                 "  policy = data.aws_iam_policy_document.{}-{}.json",
                 role.name, policy.name
-            );
-            println!("}}");
+            )?;
+            writeln!(writer, "}}")?;
 
-            print_policy_document(&format!("{}-{}", role.name, policy.name), &policy);
+            print_policy_document(writer, &format!("{}-{}", role.name, policy.name), &policy)?;
         }
         for policy in &role.attached_managed_policies {
             let short_policy_name = policy.rsplitn(2, '/').next().unwrap_or_else(|| {
@@ -135,66 +160,82 @@ pub fn print_as_hcl2(miam: &crate::Miam) {
                     policy, role.name
                 )
             });
-            println!(
+            writeln!(
+                writer,
                 r#"resource "aws_iam_role_policy_attachment" "{}-{}" {{"#,
                 role.name, short_policy_name
-            );
-            println!("  user = aws_iam_role.{}.name", role.name);
-            println!("  policy_arn = aws_iam_policy.{}.arn", short_policy_name);
-            println!("}}");
+            )?;
+            writeln!(writer, "  user = aws_iam_role.{}.name", role.name)?;
+            writeln!(
+                writer,
+                "  policy_arn = aws_iam_policy.{}.arn",
+                short_policy_name
+            )?;
+            writeln!(writer, "}}")?;
         }
     }
 
     for policy in &miam.managed_policies {
-        println!(r#"resource "aws_iam_policy" "{}" {{"#, policy.name);
-        println!(r#"  name = "{}""#, policy.name);
+        writeln!(writer, r#"resource "aws_iam_policy" "{}" {{"#, policy.name)?;
+        writeln!(writer, r#"  name = "{}""#, policy.name)?;
         if let Some(ref path) = policy.path {
-            println!(r#"  path = "{}""#, path);
+            writeln!(writer, r#"  path = "{}""#, path)?;
         }
-        println!(
+        writeln!(
+            writer,
             "  policy = data.aws_iam_policy_document.{}.json",
             policy.name
-        );
-        println!("}}");
+        )?;
+        writeln!(writer, "}}")?;
 
-        print_policy_document(&policy.name, &policy.policy_document);
+        print_policy_document(writer, &policy.name, &policy.policy_document)?;
     }
+    Ok(())
 }
 
-fn print_policy_document(name: &str, policy_document: &crate::PolicyDocument) {
-    println!(r#"data "aws_iam_policy_document" "{}" {{"#, name);
+fn print_policy_document<W>(
+    writer: &mut W,
+    name: &str,
+    policy_document: &crate::PolicyDocument,
+) -> Result<(), std::io::Error>
+where
+    W: std::io::Write,
+{
+    writeln!(writer, r#"data "aws_iam_policy_document" "{}" {{"#, name)?;
     if let Some(ref version) = policy_document.version {
-        println!(r#"  version = "{}""#, version);
+        writeln!(writer, r#"  version = "{}""#, version)?;
     }
     for statement in &policy_document.statements {
-        println!(r#"  statement {{"#);
-        println!(r#"    effect = "{}""#, statement.effect);
-        println!("    actions = {:?}", statement.actions);
-        println!(
+        writeln!(writer, r#"  statement {{"#)?;
+        writeln!(writer, r#"    effect = "{}""#, statement.effect)?;
+        writeln!(writer, "    actions = {:?}", statement.actions)?;
+        writeln!(
+            writer,
             "    resources = {:?}",
             statement
                 .resources
                 .iter()
                 .map(|s| replace_iam_interpolation(s))
                 .collect::<Vec<_>>(),
-        );
+        )?;
         for condition in &statement.conditions {
-            println!("      condition {{");
-            println!(r#"      test = "{}""#, condition.test);
-            println!(r#"      variable = "{}""#, condition.variable);
-            println!(
+            writeln!(writer, "      condition {{")?;
+            writeln!(writer, r#"      test = "{}""#, condition.test)?;
+            writeln!(writer, r#"      variable = "{}""#, condition.variable)?;
+            writeln!(
+                writer,
                 "      values = {:?}",
                 condition
                     .values
                     .iter()
                     .map(|s| replace_iam_interpolation(s))
                     .collect::<Vec<_>>()
-            );
-            println!("      }}");
+            )?;
+            writeln!(writer, "      }}")?;
         }
-        println!("  }}");
+        writeln!(writer, "  }}")?;
     }
-    println!("}}");
+    writeln!(writer, "}}")
 }
 
 fn replace_iam_interpolation(s: &str) -> String {
