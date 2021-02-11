8e5b310e7fc30769e86cdb565a4b902e80da0f89
Kohei Suzuki <eagletmt@gmail.com>
2021-02-11 16:51:32 +0900

    rust/miam2tf: Support groups

--- rust/miam2tf/mrblib/miam.rb
+++ rust/miam2tf/mrblib/miam.rb
@@ -1,7 +1,8 @@
-Root = Struct.new(:users) do
+Root = Struct.new(:users, :groups) do
   def initialize
     super
     self.users ||= []
+    self.groups ||= []
   end
 end
 
@@ -13,6 +14,14 @@ def user(name, path: nil, &block)
   user.path = path
   UserContext.new(user).instance_eval(&block)
   @root.users << user
+end
+
+def group(name, path: nil, &block)
+  group = Group.new
+  group.name = name
+  group.path = path
+  GroupContext.new(group).instance_eval(&block)
+  @root.groups << group
 end
 
 def exclude(_pattern)
@@ -36,8 +45,25 @@ class UserContext
   end
 
   def policy(name, &block)
-    raw = block.call
-    policy = PolicyDocument.new
+    @user.policies << PolicyDocument.from_raw(name, block.call)
+  end
+
+  def groups(*grps)
+    @user.groups.concat(grps.map(&:to_s))
+  end
+
+  def attached_managed_policies(*policies)
+    @user.attached_managed_policies.concat(policies.map(&:to_s))
+  end
+
+  def include_template(template_name, context = {})
+    # TODO
+  end
+end
+
+PolicyDocument = Struct.new(:name, :version, :statements) do
+  def self.from_raw(name, raw)
+    policy = new
     policy.name = name.to_s
     policy.version = raw['Version']
     statements = raw['Statement']
@@ -63,22 +89,36 @@ class UserContext
       end
       stmt
     end
-    @user.policies << policy
+    policy
+  end
+end
+PolicyStatement = Struct.new(:effect, :actions, :resources, :conditions)
+PolicyCondition = Struct.new(:test, :variable, :values)
+
+Group = Struct.new(:name, :path, :policies, :attached_managed_policies) do
+  def initialize
+    super
+    self.policies ||= []
+    self.attached_managed_policies ||= []
+  end
+end
+
+class GroupContext
+  attr_reader :group
+
+  def initialize(group)
+    @group = group
   end
 
-  def groups(*grps)
-    @user.groups.concat(grps.map(&:to_s))
+  def policy(name, &block)
+    @group.policies << PolicyDocument.from_raw(name, block.call)
   end
 
   def attached_managed_policies(*policies)
-    @user.attached_managed_policies.concat(policies.map(&:to_s))
+    @group.attached_managed_policies.concat(policies.map(&:to_s))
   end
 
   def include_template(template_name, context = {})
     # TODO
   end
 end
-
-PolicyDocument = Struct.new(:name, :version, :statements)
-PolicyStatement = Struct.new(:effect, :actions, :resources, :conditions)
-PolicyCondition = Struct.new(:test, :variable, :values)
--- rust/miam2tf/src/main.rs
+++ rust/miam2tf/src/main.rs
@@ -210,6 +210,7 @@ extern "C" fn mrb_require(
 #[derive(Debug, serde::Serialize)]
 struct Miam {
     users: Vec<User>,
+    groups: Vec<Group>,
 }
 #[derive(Debug, serde::Serialize)]
 struct User {
@@ -222,7 +223,7 @@ struct User {
 #[derive(Debug, serde::Serialize)]
 struct PolicyDocument {
     name: String,
-    version: String,
+    version: Option<String>,
     statements: Vec<PolicyStatement>,
 }
 #[derive(Debug, serde::Serialize)]
@@ -237,6 +238,63 @@ struct PolicyCondition {
     test: String,
     variable: String,
     values: Vec<String>,
+}
+#[derive(Debug, serde::Serialize)]
+struct Group {
+    name: String,
+    path: Option<String>,
+    policies: Vec<PolicyDocument>,
+    attached_managed_policies: Vec<String>,
+}
+
+fn to_rust_policy_document(
+    mrb: *mut miam2tf::mruby::mrb_state,
+    policy: miam2tf::mruby::mrb_value,
+) -> PolicyDocument {
+    let name = to_rust_string(mrb, attr_get(mrb, policy, "name\0"));
+    let version_value = attr_get(mrb, policy, "version\0");
+    let version = if mrb_nil_p(version_value) {
+        None
+    } else {
+        Some(to_rust_string(mrb, version_value))
+    };
+    let mut statements = Vec::new();
+    for statement in to_rust_iter(attr_get(mrb, policy, "statements\0")) {
+        let effect = to_rust_string(mrb, attr_get(mrb, statement, "effect\0"));
+        let mut actions = Vec::new();
+        for action in to_rust_iter(attr_get(mrb, statement, "actions\0")) {
+            actions.push(to_rust_string(mrb, action));
+        }
+        let mut resources = Vec::new();
+        for resource in to_rust_iter(attr_get(mrb, statement, "resources\0")) {
+            resources.push(to_rust_string(mrb, resource));
+        }
+        let mut conditions = Vec::new();
+        for condition in to_rust_iter(attr_get(mrb, statement, "conditions\0")) {
+            let test = to_rust_string(mrb, attr_get(mrb, condition, "test\0"));
+            let variable = to_rust_string(mrb, attr_get(mrb, condition, "variable\0"));
+            let mut values = Vec::new();
+            for value in to_rust_iter(attr_get(mrb, condition, "values\0")) {
+                values.push(to_rust_string(mrb, value));
+            }
+            conditions.push(PolicyCondition {
+                test,
+                variable,
+                values,
+            });
+        }
+        statements.push(PolicyStatement {
+            effect,
+            actions,
+            resources,
+            conditions,
+        });
+    }
+    PolicyDocument {
+        name,
+        version,
+        statements,
+    }
 }
 
 fn to_miam(
@@ -254,45 +312,7 @@ fn to_miam(
         };
         let mut policies = Vec::new();
         for policy in to_rust_iter(attr_get(mrb, user, "policies\0")) {
-            let name = to_rust_string(mrb, attr_get(mrb, policy, "name\0"));
-            let version = to_rust_string(mrb, attr_get(mrb, policy, "version\0"));
-            let mut statements = Vec::new();
-            for statement in to_rust_iter(attr_get(mrb, policy, "statements\0")) {
-                let effect = to_rust_string(mrb, attr_get(mrb, statement, "effect\0"));
-                let mut actions = Vec::new();
-                for action in to_rust_iter(attr_get(mrb, statement, "actions\0")) {
-                    actions.push(to_rust_string(mrb, action));
-                }
-                let mut resources = Vec::new();
-                for resource in to_rust_iter(attr_get(mrb, statement, "resources\0")) {
-                    resources.push(to_rust_string(mrb, resource));
-                }
-                let mut conditions = Vec::new();
-                for condition in to_rust_iter(attr_get(mrb, statement, "conditions\0")) {
-                    let test = to_rust_string(mrb, attr_get(mrb, condition, "test\0"));
-                    let variable = to_rust_string(mrb, attr_get(mrb, condition, "variable\0"));
-                    let mut values = Vec::new();
-                    for value in to_rust_iter(attr_get(mrb, condition, "values\0")) {
-                        values.push(to_rust_string(mrb, value));
-                    }
-                    conditions.push(PolicyCondition {
-                        test,
-                        variable,
-                        values,
-                    });
-                }
-                statements.push(PolicyStatement {
-                    effect,
-                    actions,
-                    resources,
-                    conditions,
-                });
-            }
-            policies.push(PolicyDocument {
-                name,
-                version,
-                statements,
-            });
+            policies.push(to_rust_policy_document(mrb, policy));
         }
         let mut groups = Vec::new();
         for group in to_rust_iter(attr_get(mrb, user, "groups\0")) {
@@ -310,7 +330,35 @@ fn to_miam(
             attached_managed_policies,
         });
     }
-    Ok(Miam { users })
+
+    let mut groups = Vec::new();
+    for group in to_rust_iter(attr_get(mrb, root, "groups\0")) {
+        let name = to_rust_string(mrb, attr_get(mrb, group, "name\0"));
+        eprintln!("processing {:?}", name);
+        let path = attr_get(mrb, group, "path\0");
+        let path = if mrb_nil_p(path) {
+            None
+        } else {
+            Some(to_rust_string(mrb, path))
+        };
+        let mut policies = Vec::new();
+        for policy in to_rust_iter(attr_get(mrb, group, "policies\0")) {
+            eprintln!("to_rust_policy_document");
+            policies.push(to_rust_policy_document(mrb, policy));
+        }
+        eprintln!("done policies");
+        let mut attached_managed_policies = Vec::new();
+        for policy in to_rust_iter(attr_get(mrb, group, "attached_managed_policies\0")) {
+            attached_managed_policies.push(to_rust_string(mrb, policy));
+        }
+        groups.push(Group {
+            name,
+            path,
+            policies,
+            attached_managed_policies,
+        });
+    }
+    Ok(Miam { users, groups })
 }
 
 fn print_as_hcl2(miam: &Miam) {
@@ -339,7 +387,9 @@ fn print_as_hcl2(miam: &Miam) {
                 r#"data "aws_iam_policy_document" "{}-{}" {{"#,
                 user.user_name, policy.name
             );
-            println!(r#"  version = "{}""#, policy.version);
+            if let Some(ref version) = policy.version {
+                println!(r#"  version = "{}""#, version);
+            }
             for statement in &policy.statements {
                 println!(r#"  statement {{"#);
                 println!(r#"    effect = "{}""#, statement.effect);
@@ -381,6 +431,67 @@ fn print_as_hcl2(miam: &Miam) {
                 user.user_name, short_policy_name
             );
             println!("  user = aws_iam_user.{}.name", user.user_name);
+            println!("  policy_arn = aws_iam_policy.{}.arn", short_policy_name);
+            println!("}}");
+        }
+    }
+
+    for group in &miam.groups {
+        println!(r#"resource "aws_iam_group" "{}" {{"#, group.name);
+        println!(r#"  name = "{}""#, group.name);
+        if let Some(ref path) = group.path {
+            println!(r#"  path = "{}""#, path);
+        }
+        println!("}}");
+
+        for policy in &group.policies {
+            println!(
+                r#"resource "aws_iam_group_policy" "{}-{}" {{"#,
+                group.name, policy.name
+            );
+            println!(r#"  name = "{}""#, policy.name);
+            println!("  user = aws_iam_group.{}.name", group.name);
+            println!(
+                "  policy = data.aws_iam_policy_document.{}-{}.json",
+                group.name, policy.name
+            );
+            println!("}}");
+
+            println!(
+                r#"data "aws_iam_policy_document" "{}-{}" {{"#,
+                group.name, policy.name
+            );
+            if let Some(ref version) = policy.version {
+                println!(r#"  version = "{}""#, version);
+            }
+            for statement in &policy.statements {
+                println!(r#"  statement {{"#);
+                println!(r#"    effect = "{}""#, statement.effect);
+                println!("    actions = {:?}", statement.actions);
+                println!("    resources = {:?}", statement.resources);
+                for condition in &statement.conditions {
+                    println!("      condition {{");
+                    println!(r#"      test = "{}""#, condition.test);
+                    println!(r#"      variable = "{}""#, condition.variable);
+                    println!("      values = {:?}", condition.values);
+                    println!("      }}");
+                }
+                println!("  }}");
+            }
+            println!("}}");
+        }
+        for policy in &group.attached_managed_policies {
+            let short_policy_name = policy.rsplitn(2, '/').next().unwrap_or_else(|| {
+                panic!(
+                    "Invalid attached_managed_policies {} found in {} group",
+                    policy, group.name
+                )
+            });
+            println!(
+                r#"resource "aws_iam_group_policy_attachment" "{}-{}" {{"#,
+                group.name, short_policy_name
+            );
+            println!("  user = aws_iam_group.{}.name", group.name);
             println!("  policy_arn = aws_iam_policy.{}.arn", short_policy_name);
             println!("}}");
         }
