cb4d3d3d19a00161ad7e87056d007ee043effed7
Kohei Suzuki <kohei-suzuki@cookpad.com>
2021-12-14 18:02:16 +0900

    Add rust/s3-du

---
+++ rust/s3-du/src/main.rs
@@ -0,0 +1,43 @@
+use structopt::StructOpt as _;
+
+#[derive(Debug, structopt::StructOpt)]
+struct Opt {
+    #[structopt(short, long)]
+    bucket: String,
+    #[structopt(short, long)]
+    prefix: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opt = Opt::from_args();
+
+    let shared_config = aws_config::load_from_env().await;
+    let s3_client = aws_sdk_s3::Client::new(&shared_config);
+
+    let mut continuation_token = None;
+    let mut size = 0;
+    loop {
+        let resp = s3_client
+            .list_objects_v2()
+            .bucket(&opt.bucket)
+            .prefix(&opt.prefix)
+            .set_continuation_token(continuation_token)
+            .send()
+            .await?;
+        continuation_token = resp.next_continuation_token;
+
+        if let Some(contents) = resp.contents {
+            for content in contents {
+                size += content.size;
+            }
+        }
+
+        if continuation_token.is_none() {
+            break;
+        }
+    }
+    println!("{}", size);
+
+    Ok(())
+}
