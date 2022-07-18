af2c388a7ea4ec41550671198592dc9b23f79252
Kohei Suzuki <eagletmt@gmail.com>
2022-07-18 12:42:13 +0900

    rust/errno: Use once_cell instead of lazy_static

--- rust/errno/build.rs
+++ rust/errno/build.rs
@@ -1,6 +1,6 @@
-lazy_static::lazy_static! {
-    static ref MACROS: std::sync::Mutex<std::cell::RefCell<Vec<(String, i32)>>> = Default::default();
-}
+#[allow(clippy::type_complexity)]
+static MACROS: once_cell::sync::OnceCell<std::sync::Mutex<std::cell::RefCell<Vec<(String, i32)>>>> =
+    once_cell::sync::OnceCell::new();
 
 #[derive(Debug, Default)]
 struct MacroCollector {}
@@ -9,6 +9,8 @@ impl bindgen::callbacks::ParseCallbacks for MacroCollector {
     fn int_macro(&self, name: &str, value: i64) -> Option<bindgen::callbacks::IntKind> {
         if name.starts_with('E') {
             MACROS
+                .get()
+                .unwrap()
                 .lock()
                 .unwrap()
                 .borrow_mut()
@@ -20,6 +22,7 @@ impl bindgen::callbacks::ParseCallbacks for MacroCollector {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     use std::io::Write as _;
+    MACROS.set(Default::default()).unwrap();
 
     bindgen::builder()
         .header_contents("wrapper.h", "#include <errno.h>")
@@ -30,7 +33,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = std::env::var("OUT_DIR")?;
     let mut file = std::fs::File::create(std::path::Path::new(&out_dir).join("errno.rs"))?;
     writeln!(file, "pub const ERROR_NUMBERS: &[(&str, i32, &str)] = &[")?;
-    for (name, value) in MACROS.lock()?.borrow().iter() {
+    for (name, value) in MACROS.get().unwrap().lock()?.borrow().iter() {
         let message = unsafe { std::ffi::CStr::from_ptr(libc::strerror(*value)).to_str()? };
         writeln!(file, "(\"{}\", {}, \"{}\"),", name, value, message)?;
     }
