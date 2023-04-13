5447136d32e210a3ec92fcfb140dffa6a6d06f7a
Kohei Suzuki <eagletmt@gmail.com>
2023-04-14 00:44:50 +0900

    Remove rust/sensors-zabbix and slack-slash-command-sample

--- rust/slack-slash-command-sample/bin/slack-slash-command-sample.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { SlackSlashCommandSampleStack } from '../lib/slack-slash-command-sample-stack';
-
-const app = new cdk.App();
-new SlackSlashCommandSampleStack(app, 'SlackSlashCommandSampleStack');
